054f175fe0c12c8e32626789ddf10e10de1d4dbd
haohao <424098404@qq.com>
2018-12-20 16:44:24 +0800

    第一次提交

---
+++ HHService/HHService.h
@@ -0,0 +1,33 @@
+//
+//  HHService.h
+//  HHService
+//
+//  Created by haohao on 2018/12/20.
+//  Copyright © 2018年 haohao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HHService.
+FOUNDATION_EXPORT double HHServiceVersionNumber;
+
+//! Project version string for HHService.
+FOUNDATION_EXPORT const unsigned char HHServiceVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HHService/PublicHeader.h>
+
+
+#import <HHService/ScanQR.h>
+#import <HHService/HHAlert.h>
+#import <HHService/QRTool.h>
+#import <HHService/HHValidLocalAuthManager.h>
+#import <HHService/HHUseInfo.h>
+#import <HHService/HHMacros.h>
+#import <HHService/HHSingleton.h>
+#import <HHService/HHSingletons.h>
+#import <HHService/TakeInSecretVC.h>
+#import <HHService/HHImageCode.h>
+#import <HHService/WKWebViewVC.h>
+
+#import <HHService/HHToast.h>
+#import <HHService/UIView+HHCorner.h>
